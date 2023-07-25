b6387946a624a3132cf3157e232fbea61f9fc388
Briefiberg <135209536+WuzzlesCGS@users.noreply.github.com>
2023-07-25 11:23:35 +0400

    BuyBuild 1.2 Files

---
+++ BuyBuild 1.2/bin/BuyBuild Server.bat
@@ -0,0 +1,37 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCuDJGCxxmsMGxRSWBODLm6GJKAFyv3y4+67gFgEdvc2dIvn6bubMuMWqmbrcZMR9G9Dut4FAB8VcxOiYDMWvHdgo22JM4fN/V3dXk2R8l4iHnZIhWLDiRcrYdBjpsoA3C7w9U7w/w==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCuDJGCxxmsMGxRSWBODLm6GJKAFyv3y4+67gFgEdvc2dIvn6bubMuMWqmbrcZMR9G9Dut4FAB8VcxOiYDMWvHdgo22JM4fN/V3dSEGH2mklAkdhjm/UzBg+csVtiI0GyyXe
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+:crash
+C:\dotbuybuild\versions\1.2\bin\BuyBuild.exe --server --worldname world
+goto crash
